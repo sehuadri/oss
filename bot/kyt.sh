118de1572bf3cf0ee4fcd3e25444d0895f011e7f
sehuadri <sehuadri@gmail.com>
2025-02-21 23:57:23 +0700

    Update kyt.sh

--- bot/kyt.sh
+++ bot/kyt.sh
@@ -20,13 +20,13 @@ apt update && apt upgrade
 apt install neofetch -y
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/sehuadri/oss/main/Bot/bot.zip
+wget https://raw.githubusercontent.com/sehuadri/oss/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/sehuadri/oss/main/Bot/kyt.zip
+wget https://raw.githubusercontent.com/sehuadri/oss/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
