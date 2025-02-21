35299d117ba01bfdad6f30ef4912aaeb4425305f
sehuadri <sehuadri@gmail.com>
2025-02-21 23:09:29 +0700

    Update ins-xray.sh

--- install/ins-xray.sh
+++ install/ins-xray.sh
@@ -11,7 +11,8 @@ CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 # ==========================================
 # Getting
-REPO="http://myrid.my.id/os/"
+REPO="https://raw.githubusercontent.com/sehuadri/oss/main/"
+REPO2="https://raw.githubusercontent.com/sehuadri/oss/main/"
 echo -e "
 "
 date
