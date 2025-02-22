a739a6b28a35f1abeea51275a792a09a1ac4c6e6
sehuadri <sehuadri@gmail.com>
2025-02-22 14:25:22 +0700

    Update autocpu.sh

--- install/autocpu.sh
+++ install/autocpu.sh
@@ -3,17 +3,17 @@ biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
 NC="\e[0m"
 RED="\033[0;31m"
 WH='\033[1;37m'
-ipsaya=$(wget -qO- ifconfig.me)
+ipsaya=$(curl -sS ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/myridwan/izinvps/ipuk/ipx"
+data_ip="https://raw.githubusercontent.com/awanklod/izin_new/main/ip"
 checking_sc() {
     useexp=$(curl -sS "$data_ip" | grep "$ipsaya" | awk '{print $3}')
     date_list=$(date +%Y-%m-%d)
 
     if [[ $(date -d "$date_list" +%s) -lt $(date -d "$useexp" +%s) ]]; then
         echo -e " [INFO] Fetching server version..."
-        REPO="http://myrid.my.id/os/" # Ganti dengan URL repository Anda
+        REPO="https://raw.githubusercontent.com/sehuadri/oss/main/" # Ganti dengan URL repository Anda
         serverV=$(curl -sS ${REPO}versi)
 
         if [[ -f /opt/.ver ]]; then
@@ -27,7 +27,7 @@ checking_sc() {
             return
         else
             echo -e " [INFO] Versi script berbeda. Memulai proses update script..."
-            wget -q http://myrid.my.id/os/menu/update.sh -O update.sh
+            wget -q https://raw.githubusercontent.com/sehuadri/oss/main/menu/update.sh -O update.sh
             chmod +x update.sh
             ./update.sh
             echo $serverV > /opt/.ver.local
@@ -42,8 +42,8 @@ checking_sc() {
         echo -e "   \033[0;33mYour VPS\033[0m $ipsaya \033[0;33mHas been Banned\033[0m"
         echo -e "     \033[0;33mBuy access permissions for scripts\033[0m"
         echo -e "             \033[0;33mContact Admin :\033[0m"
-        echo -e "      \033[2;32mWhatsApp\033[0m wa.me/6287769315399"
-        echo -e "      \033[2;32mTelegram\033[0m t.me/kytxz"
+        echo -e "      \033[2;32mWhatsApp\033[0m wa.me/none"
+        echo -e "      \033[2;32mTelegram\033[0m t.me/amiqyu"
         echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
 
         for service in nginx kyt xray ws haproxy; do
@@ -80,7 +80,7 @@ checking_sc() {
 checking_sc
 cd
 today=$(date -d "0 days" +"%Y-%m-%d")
-Exp2=$(curl -sS https://raw.githubusercontent.com/myridwan/izinvps/ipuk/ipx | grep $ipsaya | awk '{print $3}')
+Exp2=$(curl -sS https://raw.githubusercontent.com/awanklod/izin_new/main/ip | grep $ipsaya | awk '{print $3}')
 d1=$(date -d "$Exp2" +%s)
 d2=$(date -d "$today" +%s)
 certificate=$(( (d1 - d2) / 86400 ))
