5f27c60312a261e9e473e36f4895fe37faaa34ee
sehuadri <sehuadri@gmail.com>
2025-02-25 03:29:39 +0700

    Update websocketku.sh

--- sshws/websocketku.sh
+++ sshws/websocketku.sh
@@ -1,3 +1,25 @@
+#!/bin/bash
+# Proxy For Edukasi & Imclass
+file_path="/etc/handeling"
+
+# Cek apakah file ada
+if [ ! -f "$file_path" ]; then
+    # Jika file tidak ada, buat file dan isi dengan dua baris
+    echo -e "cloudvpn premium server\nBLUE" | sudo tee "$file_path" > /dev/null
+    echo "File '$file_path' berhasil dibuat."
+else
+    # Jika file ada, cek apakah isinya kosong
+    if [ ! -s "$file_path" ]; then
+        # Jika file ada tetapi kosong, isi dengan dua baris
+        echo -e "cloudvpn premium server\nBlue" | sudo tee "$file_path" > /dev/null
+        echo "File '$file_path' kosong dan telah diisi."
+    else
+        # Jika file ada dan berisi data, tidak lakukan apapun
+        echo "File '$file_path' sudah ada dan berisi data."
+    fi
+fi
+# Link Hosting Kalian
+sudo apt install python3
 REPO="https://raw.githubusercontent.com/Adidastore11/adi2/main/"
 wget -O /usr/bin/ws "${REPO}files/ws" >/dev/null 2>&1
     wget -O /usr/bin/tun.conf "${REPO}config/tun.conf" >/dev/null 2>&1
