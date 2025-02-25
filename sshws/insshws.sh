2f43f3bd5eb4525472be00259921099e80e6c1dd
sehuadri <sehuadri@gmail.com>
2025-02-25 23:45:58 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -20,15 +20,58 @@ else
 fi
 # Link Hosting Kalian
 sudo apt install python3
-REPO="https://raw.githubusercontent.com/Adidastore11/adi2/main/"
-wget -O /usr/bin/ws "${REPO}files/ws" >/dev/null 2>&1
-    wget -O /usr/bin/tun.conf "${REPO}config/tun.conf" >/dev/null 2>&1
-    wget -O /etc/systemd/system/ws.service "${REPO}files/ws.service" >/dev/null 2>&1
-    chmod +x /etc/systemd/system/ws.service
-    chmod +x /usr/bin/ws
-    chmod 644 /usr/bin/tun.conf
-systemctl disable ws
-systemctl stop ws
-systemctl enable ws
-systemctl start ws
-systemctl restart ws
+
+wget -O /usr/local/bin/ws "https://raw.githubusercontent.com/sehuadri/oss/main/sshws/ws"
+chmod +x /usr/local/bin/ws
+
+# Installing Service
+cat > /etc/systemd/system/ws.service << END
+[Unit]
+Description=Proxy Mod By Newbie Store 
+Documentation=https://t.me/newbie_store24
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/ws
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws.service
+systemctl start ws.service
+systemctl restart ws.service
+
+wget -O /usr/local/bin/ws-ovpn "https://raw.githubusercontent.com/sehuadri/oss/main/os/sshws/ws"
+chmod +x /usr/local/bin/ws-ovpn
+
+# Installing Service
+cat > /etc/systemd/system/ws-ovpn.service << END
+[Unit]
+Description=Proxy Mod By NEWBIE STORE
+Documentation=https://t.me/newbie_store24
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/ws-ovpn 2086
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+systemctl daemon-reload
+systemctl enable ws-ovpn
+systemctl restart ws-ovpn
