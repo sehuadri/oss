b26d4ab8d7045a0f571b4c5def87b6e435230fe5
sehuadri <sehuadri@gmail.com>
2025-02-25 10:34:21 +0700

    Update websocketku.sh

--- sshws/websocketku.sh
+++ sshws/websocketku.sh
@@ -32,7 +32,3 @@ systemctl stop ws
 systemctl enable ws
 systemctl start ws
 systemctl restart ws
-wget -q -O /usr/local/share/xray/geosite.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat" >/dev/null 2>&1
-wget -q -O /usr/local/share/xray/geoip.dat "https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat" >/dev/null 2>&1
-wget -O /usr/sbin/ftvpn "${REPO}files/ftvpn" >/dev/null 2>&1
-chmod +x /usr/sbin/ftvpn
