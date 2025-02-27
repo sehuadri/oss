819c0d699fa0038451b3d33bd6528c8e8778b72d
sehuadri <sehuadri@gmail.com>
2025-02-27 09:43:43 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -5,13 +5,13 @@ file_path="/etc/handeling"
 # Cek apakah file ada
 if [ ! -f "$file_path" ]; then
     # Jika file tidak ada, buat file dan isi dengan dua baris
-    echo -e "cloudvpn premium server\nBLUE" | sudo tee "$file_path" > /dev/null
+    echo -e "CLOUDVPN SEREVR PREMIUM\nCyan" | sudo tee "$file_path" > /dev/null
     echo "File '$file_path' berhasil dibuat."
 else
     # Jika file ada, cek apakah isinya kosong
     if [ ! -s "$file_path" ]; then
         # Jika file ada tetapi kosong, isi dengan dua baris
-        echo -e "cloudvpn premium server\nBlue" | sudo tee "$file_path" > /dev/null
+        echo -e "CLOUDVPN SEREVR PREMIUM\nCyan" | sudo tee "$file_path" > /dev/null
         echo "File '$file_path' kosong dan telah diisi."
     else
         # Jika file ada dan berisi data, tidak lakukan apapun
